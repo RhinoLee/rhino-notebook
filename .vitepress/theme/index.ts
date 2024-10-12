b8dc17df000c86daa72e2ee16bef47b1f73a1a32
Rhino Lee <picklerhino@gmail.com>
2024-10-12 16:53:44 +0800

    unocss

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+import DefaultTheme from 'vitepress/theme'
+import './styles/main.css'
+import './styles/demo.css'
+import './styles/utils.css'
+import './styles/vars.css'
+import './styles/overrides.css'
+import 'uno.css'
+
+export default {
+  ...DefaultTheme,
+}
